eb0456b38d32e465e42cf16d13a5c89a3c314dfc
George Sánchez Tomasto <george.sanchez_consultor@mdp.com.pe>
2024-02-08 18:40:56 -0500

    [CREATE]: Create Inicitial Proyect, Modify AppSetting.json and create ConnectionStrings, Add Package System.Data.Sql for to Solve, Create Model User, Create UsuarioController and Create Endpoint GetAll using ADO.net

---
+++ ApiUsuarios/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using ApiUsuarios.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ApiUsuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        public readonly string? connection;
+        public UsuarioController(IConfiguration configuration)
+        {
+            connection = configuration.GetConnectionString("DefaultConnection");
+        }
+        [HttpGet]
+        public IEnumerable<Usuario> GetAll()
+        {
+            List<Usuario> usuarios = new ();
+            using (SqlConnection sqlConnection = new(connection))
+            {
+                sqlConnection.Open();
+                using (SqlCommand cmd = new("SP_MostrarUsuario", sqlConnection))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Usuario usuario = new Usuario {
+                                Id = Convert.ToInt32(reader["ID"]),
+                                Nombres = reader["Nombres"].ToString(),
+                                Apellidos = reader["Apellidos"].ToString()
+                            };
+                            usuarios.Add(usuario);
+                        }
+                    }
+                }
+            }
+            return usuarios;
+        }
+    }
+}
---
+++ ApiUsuarios/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiUsuarios.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+    }
+}
